314d83aac593bb0af5dbe9d4c6aeefcbba5b1662
Dara Breen <darabreen.wexford@gmail.com>
2012-06-24 18:35:45 +0100

    sample data

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'sqlite3'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem "sass-rails", "~> 3.2.4"
+  gem "coffee-rails", "~> 3.2.2"
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
+
+gem 'spree', '1.1.1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinyscent::Application.config.secret_token = '7ec2ceba282992a91fc5fd85d59971e6d8770c48333e3b8606351c7a71d2b69a3c4d4a5c68ef77bfe12c2963781c08991550b7062607d95bee47b5ed31401e81'
